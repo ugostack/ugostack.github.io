f0a23a64b3eccc13ec5c7effe3aedc6033be3e15
ugostack <ugo+github@sanfilippo.be>
2022-07-01 22:55:55 +0200

    update

---
+++ my-app/src/lib/components/Navbar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+
+</script>
+
+<li>
+    <ul>Qui suis-je ?</ul>
+    <ul>Mes compétances</ul>
+    <ul>Mes projets</ul>
+    <ul>Langages</ul>
+</li>
+
+<style>
+    li{
+        display: flex;
+    }
+
+    ul{
+        font-weight: bold;
+    }
+</style>
